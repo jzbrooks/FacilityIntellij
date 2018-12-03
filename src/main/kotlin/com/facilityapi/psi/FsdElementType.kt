f5b7c7681d23de6cb9b5e7e535a3b1ffb91ec8c8
Justin Brooks <justin.brooks@faithlife.com>
2018-12-03 11:33:39 -0800

    Add dummy flex definition

--- src/main/kotlin/com/facilityapi/psi/FsdElementType.kt
+++ src/main/kotlin/com/facilityapi/psi/FsdElementType.kt
@@ -3,4 +3,4 @@ package com.facilityapi.psi
 import com.facilityapi.FsdLanguage
 import com.intellij.psi.tree.IElementType
 
-class FsdElementType(val debugName: String) : IElementType(debugName, FsdLanguage)
+data class FsdElementType(val debugName: String) : IElementType(debugName, FsdLanguage)
