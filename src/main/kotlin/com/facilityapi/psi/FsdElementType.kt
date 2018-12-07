696bd4f8de4aa140f2187443c8e5b8252ce36893
Justin Brooks <justin.brooks@faithlife.com>
2018-12-06 19:10:02 -0800

    cleanup bnf

--- src/main/kotlin/com/facilityapi/psi/FsdElementType.kt
+++
@@ -1,6 +0,0 @@
-package com.facilityapi.psi
-
-import com.facilityapi.FsdLanguage
-import com.intellij.psi.tree.IElementType
-
-class FsdElementType(val debugName: String) : IElementType(debugName, FsdLanguage)
